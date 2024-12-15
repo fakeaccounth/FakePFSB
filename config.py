3613ece51571b851c9aad82df12b2d0de024ba64
rohit3607 <rohitplayer87089@gmail.com>
2024-12-15 11:21:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "filesx")
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002485124074"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002215102799"))
 JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
