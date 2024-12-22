c7b2d97a708270b8bd6f7cebe6d5ff0e46f855d7
rohit3607 <rohitplayer87089@gmail.com>
2024-12-22 17:59:27 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,6 +1,6 @@
 from bot import Bot
+from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
@@ -12,7 +12,13 @@ async def stats(bot: Bot, message: Message):
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
+
+
 @Bot.on_message(filters.private & filters.incoming)
-async def useless(_,message: Message):
+async def useless(_, message: Message):
+    # Check if the user is in the admin list
+    if message.from_user.id in ADMINS:
+        return  # Don't reply if the user is an admin
+
     if USER_REPLY_TEXT:
         await message.reply(USER_REPLY_TEXT)
