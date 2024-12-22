bed11d3c8d26e829cf2edb087b9e00edff11970d
rohit3607 <rohitplayer87089@gmail.com>
2024-12-22 16:18:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -65,6 +65,12 @@ WEBSITE_URL_MODE = os.environ.get('WEBSITE_URL_MODE', True) # Set True or False
 # If Website Url Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 WEBSITE_URL = os.environ.get("WEBSITE_URL", "https://rohittgcoder.blogspot.com/2024/12/rohit18.html") 
 
+# Turn this feature on or off using True or False put value inside  ""
+# TRUE for yes FALSE if no 
+USE_SHORTLINK = True if os.environ.get('USE_SHORTLINK', "TRUE") == "TRUE" else False 
+# only shareus service known rightnow rest you can test on your own
+SHORTLINK_API_URL = os.environ.get("SHORTLINK_API_URL", "Modijiurl.com")
+SHORTLINK_API_KEY = os.environ.get("SHORTLINK_API_KEY", "beb3b795a226177f3af7c937a2f01d5d7d4f4cf0")
 
 #Set true if you want Disable your Channel Posts Share button
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
