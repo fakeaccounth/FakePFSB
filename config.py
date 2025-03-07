ab12b6e1c96949684de966c5a5ce025025d7d23a
fakeaccounth <sumitkbdlitani123@gmail.com>
2025-03-07 21:30:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,11 +30,11 @@ PORT = os.environ.get("PORT", "2728")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "MONGONOOB")
+DB_NAME = os.environ.get("DATABASE_NAME", "")
 
 
 #force sub channel id, if you want enable force sub
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002350773304"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 JOIN_REQS_DB = os.environ.get("JOIN_REQS_DB", DB_URI)
 FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 
