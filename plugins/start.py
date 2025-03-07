7291fcd3078184d0484ed664cec9b95a1e77ba7b
rohit3607 <rohitplayer87089@gmail.com>
2025-03-07 17:19:07 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -142,24 +142,12 @@ REPLY_ERROR = """<code>Use this command as a replay to any telegram message with
 
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
-
-    if bool(JOIN_REQUEST_ENABLE):
-        invite = await client.create_chat_invite_link(
-            chat_id=FORCE_SUB_CHANNEL,
-            creates_join_request=True
-        )
-        ButtonUrl = invite.invite_link
-    else:
-        ButtonUrl = client.invitelink
-
     buttons = [
         [
-            InlineKeyboardButton(
-                "Join Channel",
-                url = ButtonUrl)
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink),
+            InlineKeyboardButton(text="Join Channel", url=client.invitelink2),
         ]
     ]
-
     try:
         buttons.append(
             [
@@ -173,18 +161,18 @@ async def not_joined(client: Client, message: Message):
         pass
 
     await message.reply(
-    text=FORCE_MSG.format(
-        first=message.from_user.first_name,
-        last=message.from_user.last_name,
-        username=None if not message.from_user.username else '@' + message.from_user.username,
-        mention=message.from_user.mention,
-        id=message.from_user.id
-    ),
-    reply_markup=InlineKeyboardMarkup(buttons),
-    quote=True,
-    disable_web_page_preview=True,
-    message_effect_id=5104841245755180586  # Add the effect ID here
-)
+        text = FORCE_MSG.format(
+                first = message.from_user.first_name,
+                last = message.from_user.last_name,
+                username = None if not message.from_user.username else '@' + message.from_user.username,
+                mention = message.from_user.mention,
+                id = message.from_user.id
+            ),
+        reply_markup = InlineKeyboardMarkup(buttons),
+        quote = True,
+        disable_web_page_preview = True
+    )
+
 
 @Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
 async def get_users(client: Bot, message: Message):
