591c6e5815980208cd6edf01cbfa185837e923ea
fakeaccounth <sumitkbdlitani123@gmail.com>
2025-03-07 19:37:56 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,7 +29,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 PORT = os.environ.get("PORT", "2728")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://MONGONOOB:MONGONOOB@cluster0.7mj8fw2.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "")
 
 
