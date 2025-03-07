dc8040fe6e9e819e9bf102212e3491a5d68b9339
fakeaccounth <sumitkbdlitani123@gmail.com>
2025-03-07 21:41:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -29,8 +29,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", ""))
 PORT = os.environ.get("PORT", "2728")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "")
-DB_NAME = os.environ.get("DATABASE_NAME", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://MONGONOOB:MONGONOOB@cluster0.7mj8fw2.mongodb.net/?retryWrites=true&w=majority")
+DB_NAME = os.environ.get("DATABASE_NAME", "MONGONOOB")
 
 
 #force sub channel id, if you want enable force sub
