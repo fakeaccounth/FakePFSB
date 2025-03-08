c4525471394ff4b6b1b4d8471710f36eb8f9eb4a
fakeaccounth <sumitkbdlitani123@gmail.com>
2025-03-08 13:35:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "MONGONOOB")
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", ""))
 JOIN_REQS_DB = os.environ.get("JOIN_REQS_DB", DB_URI)
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "-1002415980807"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL2", "0"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
 
